26aa216f3a5d72ba2dbe12422a49dda187a6b430
Arun Adla <a.arun2000@outlook.com>
2024-07-11 00:31:03 +0530

    added student routes declaration

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const cookieParser =require('cookie-parser')
 
 const connectDB=require('./Admin/DB/connect')
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 5000
 // const subjectObj=require("./Admin/Models/subject.model")
 
 app.use(cors({
@@ -29,6 +29,8 @@ const subjectRouter=require("./Admin/Routes/subject.routes.js");
 const semesterRouter=require("./Admin/Routes/semester.routes.js");
 //import faculty routes
 const facultyRouter = require("./Faculty/Routes/routes.js");
+//import student routes
+const studentRouter = require("./Student/Routes/routes.js")
 
 
 
@@ -42,13 +44,18 @@ app.use("/api/v2/semester",semesterRouter)
 
 app.use("/api/v2/faculty/login",facultyRouter);
 
+//student routes declaration
+
+app.use("/api/v2/student/login",studentRouter);
+
 
 //Server and DataBase connection 
 const start = async () => {
     try {
+      const uri = process.env.MONGO_URI
       await connectDB(process.env.MONGO_URI);
        
-        console.log("Connected to Database")
+      console.log("Connected to Database");
 
       app.listen(PORT, () =>
         console.log(`Server is listening on port ${PORT}...`)
