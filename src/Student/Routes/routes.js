675c921d4edca85b660c8dad1b2d8c4e08ede306
Arun Adla <a.arun2000@outlook.com>
2024-07-11 23:40:47 +0530

    added check attendance route

--- src/Student/Routes/routes.js
+++ src/Student/Routes/routes.js
@@ -4,10 +4,12 @@ const router=express.Router()
 // controllers
 
 const {updatePassword} = require("../Controllers/updatePassword.controller.js")
+const {checkAttendance} = require("../Controllers/checkAttendance.controller.js")
 
 
 // routes
 
 router.route("/password").post(updatePassword)
+router.route("/attendance").post(checkAttendance)
 
 module.exports  = router;
