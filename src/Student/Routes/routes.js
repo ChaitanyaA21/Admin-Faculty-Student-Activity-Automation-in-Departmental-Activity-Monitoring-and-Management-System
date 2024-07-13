d8dde9b72a6ca127d7da568f161b8f1c43d50b65
Arun Adla <a.arun2000@outlook.com>
2024-07-13 15:31:09 +0530

    updated with check profile route

--- src/Student/Routes/routes.js
+++ src/Student/Routes/routes.js
@@ -7,6 +7,7 @@ const { updatePassword } = require("../Controllers/updatePassword.controller.js"
 const { checkAttendance } = require("../Controllers/checkAttendance.controller.js");
 const { updateContact } = require('../Controllers/updateContact.controller.js');
 const { checkInternalMarks } = require('../Controllers/checkInternalMarks.controller.js');
+const { checkProfile } = require('../Controllers/checkProfile.controller.js');
 
 
 // routes
@@ -15,5 +16,6 @@ router.route("/password").post(updatePassword)
 router.route("/attendance").post(checkAttendance)
 router.route("/updatecontact").post(updateContact)
 router.route("/internalmarks").post(checkInternalMarks)
+router.route("/profile").post(checkProfile)
 
 module.exports  = router;
